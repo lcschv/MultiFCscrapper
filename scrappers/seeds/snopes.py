08e2f59d63bb27ae16c97d196e5529388db0216e
Lucas Chaves Lima <lcs.chv@gmail.com>
2018-09-21 01:08:21 +0200

    Finished scrapper for website snopes.

--- scrappers/seeds/snopes.py
+++ scrappers/seeds/snopes.py
@@ -1,9 +1,5 @@
 from scrappers.Commons import *
-import os
 from bs4 import BeautifulSoup
-import re
-from bs4.element import NavigableString
-import string
 
 class Snopes(Commons):
     def __init__(self, seed_id, seed_url, drive_path="scrappers\seeds\chromedriver.exe"):
@@ -109,34 +105,3 @@ class Snopes(Commons):
 if __name__ == '__main__':
     snopes = Snopes(1, "https://www.snopes.com/fact-check/page/", "C:\Lucas\PhD\CredibilityDataset\scrappers\seeds\chromedriver.exe")
     snopes.start()
-# def analyze_forbes_page():
-#
-#     pass
-#
-#
-# def check_next_page(url):
-#     try to open url + number
-#     url = "https://www.snopes.com/fact-check/page/"
-#
-#
-#
-
-# def get_all_claims_from_page():
-#     pass
-#
-# #This is the main function of the Snopes Scrapper, the one that must be added to the function_mappings dictionary
-# def snopes(self, url):
-#     destination_folder = "C:\Lucas\PhD\CredibilityDataset\data\seeds\\1\html_claims"
-#     for i in range(1, 1021):
-#         print (i)
-#         get_all_claims_from_page()
-
-    # content = self._get_full_doc_(url)
-    # filepath = destination_folder+ i +".txt"
-    # self.write_webpage_content_tofile(content, destination_folder)
-
-
-#         analyze_forbes_page()
-# #
-#     scrap = new Scrapper("https://www.snopes.com/fact-check/page/")
-#     scrap._get_full_doc_(url+1)
