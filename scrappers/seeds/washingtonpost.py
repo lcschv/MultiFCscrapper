d4a616a1fa096cbc911bd330fc677872c4195a26
Lucas Chaves Lima <lcs.chv@gmail.com>
2018-10-02 18:34:26 +0200

    Washingtonpost scrapper semi-final version (missing just the label).

--- scrappers/seeds/washingtonpost.py
+++ scrappers/seeds/washingtonpost.py
@@ -14,45 +14,39 @@ class WashingtonPost(Commons):
         self.dict_claims_category = {}
         self.claim_num = 1
 
-    def get_claim(self, soup, url):
-        claim = ""
-        div_Tag = soup.find("div",{"class":"inline-content wysiwyg right"})
-        if div_Tag is not None:
-            ul = div_Tag.find("ul")
-            if ul is not None:
-                claim = ul.find("li").text.strip()
-            else:
-                p = soup.find("p", {"class": "first"})
-                if p is not None:
-                    claim = p.text.strip()
-            # print ("Veredict:", ul.find("li").text.strip())
-        else:
-            p = soup.find("p", {"class":"first"})
-            if p is not None:
-                claim = p.text.strip()
+    def get_share_statements(self, soup, url):
+        claim = []
+        claim = soup.find_all("div",{"class":"sharethefacts"})
         return claim
 
     def get_claim_label(self, soup, url):
         label = ""
-        div_tag = soup.find_all("div",{"class":"rating-wrapper"})
+        div_tag = soup.find_all("h3")
         try:
-            for tag in div_tag:
-                aTags = tag.find_all("a")
-                for a_tag in aTags:
-                    label = a_tag.find("span").text.strip()
+            for i in range(len(div_tag)):
+                if div_tag[i].text == "The Pinocchio Test":
+                    label = div_tag[i+1].text
         except Exception as e:
             # self.cont_errors += 1
             print(str(e), "wrong label at url: ", url)
             label = "wrong label"
         return label
 
-    def get_publish_date(self, soup, url):
-        # publish_date = soup.find("div",{"class":"post-box-meta-single"}).find("span").text.strip()
-        publish_date = soup.find("span",{"class":"print"}).text.strip()
-        return publish_date
+    def get_claim_date_published(self, soup, url):
+        claim_date = ""
 
-    def get_author(self):
-        pass
+        div_share = soup.find("div", {"class": "sharethefacts-dateline"})
+        if div_share is not None:
+            claim_date = div_share.text.rsplit('–')[1]
+        return claim_date
+
+    def get_checker(self, soup, url):
+        author = ""
+        author_info = soup.find("a",{"class":"author-name"})
+        if author_info is not None:
+            author = author_info.text.strip()
+        # author = author_info.find('a').text
+        return author
 
     def get_claim_date(self):
         pass
@@ -106,70 +100,98 @@ class WashingtonPost(Commons):
 
     def get_article_title(self, soup, url):
         title=""
-        title= soup.find('div',{"class":"article section"}).find('h1').text.strip()
+        title= soup.find('div',{"class":"topper-headline"}).find('h1').text.strip()
         # title = entire_title.rsplit('-',1)[0]
         # label = entire_title.rsplit('-', 1)[1]
         return title
 
+    def get_speaker(self, statement, url):
+        speaker = ""
+        div_share = statement.find("div",{"class": "sharethefacts-speaker-name"})
+        if div_share is not None:
+            speaker = div_share.text.strip()
+        return speaker
+
+    def get_publish_date(self, soup, claim_url):
+        publish_date = ""
+        span = soup.find("span",{"class":"author-timestamp"})
+        if span is not None:
+            publish_date = span.text.strip()
+        return publish_date
+
+    def get_claim(self, sharefact, claim_url):
+        claim = ""
+        statement = sharefact.find("div",{"class":"sharethefacts-statement"})
+        if statement is not None:
+            claim = statement.text.strip()
+        return claim
 
     def parse_claim_url(self):
+        i = 1
         for claim_id, claim_url in self.dict_claims_urls.items():
             html = self._get_full_doc_(claim_url)
             soup = BeautifulSoup(html, 'html.parser')
             self.clean_soup(soup)
-            ###THIS IS ASSUMING THAT THE LABEL IS DIVIDED INTO 3, however it's not just that.
-            label = self.dict_claims_category[claim_url]
-            # article_title, label = self.get_article_title_and_label(soup, claim_url)
-            publish_date = self.get_publish_date(soup,claim_url)
-            claim = self.get_claim(soup, claim_url)
-            article_title = self.get_article_title(soup, claim_url)
-            # label = self.get_claim_label(soup, claim_url)
-            # author, publish_date =self.article_info(soup, claim_url)
-            # category = self.get_claim_category(soup, claim_url)
-            tags = self.get_tags(soup, claim_url)
-            category = self.get_tags(soup, claim_url)
-            reason = self.get_reason(soup, claim_url)
-            # # print (claim_id, claim, label, article_title, author, publish_date)
-            claim_object = ClaimSchema()
 
-            claim_object.set_id(claim_id)
-            claim_object.set_claim_url(claim_url)
-            claim_object.set_claim(claim)
-            claim_object.set_label(label)
-            claim_object.set_article_title(article_title)
-            claim_object.set_categories(category)
-            claim_object.set_reason(reason)
-            # # claim_object.set_checker(author)
-            claim_object.set_publish_date(publish_date)
-            # # claim_object.set_reason(author)
-            claim_object.set_tags(tags)
-            #
-            claim_object.pretty_print()
+            # label = self.dict_claims_category[claim_url]
+            # article_title, label = self.get_article_title_and_label(soup, claim_url)
+            publish_date = self.get_publish_date(soup, claim_url)
+            share_statements = self.get_share_statements(soup, claim_url)
+            if len(share_statements) > 0:
+                checker = self.get_checker(soup, claim_url)
+                article_title = self.get_article_title(soup, claim_url)
+                category = self.dict_claims_category[claim_url]
+
+                for sharefact in share_statements:
+                    # article_title = self.get_article_title(soup, claim_url)
+                    # label = self.get_claim_label(soup, claim_url)
+
+                    speaker = self.get_speaker(sharefact, claim_url)
+                    claim = self.get_claim(sharefact, claim_url)
+
+                    claim_date = self.get_claim_date_published(sharefact, claim_url)
+
+                    claim_object = ClaimSchema()
+
+                    claim_object.set_id(str(i))
+                    claim_object.set_claim_url(claim_url)
+                    claim_object.set_claim(claim)
+                    # claim_object.set_label(label)
+                    claim_object.set_article_title(article_title)
+                    claim_object.set_categories(category)
+                    claim_object.set_claim_date(claim_date)
+                    claim_object.set_speaker(speaker)
+                    # claim_object.set_reason(reason)
+                    claim_object.set_checker(checker)
+                    claim_object.set_publish_date(publish_date)
+                    # claim_object.set_tags(tags)
+                    #
+                    i+=1
+                    claim_object.pretty_print()
 
     def click_on_load_more(self):
         self.driver.get("https://www.washingtonpost.com/news/fact-checker/")
         time.sleep(10)
-        while True:
+        i=1
+        while True and i<2:
             # self.driver.implicitly_wait(10)
             try:
                 self.driver.find_element_by_class_name('pb-loadmore').click()
+                i+=1
                 time.sleep(0.5)
             except:
                 break
 
-    def get_list_claims_url(self, soup, category):
-        ulTags = soup.findAll("ul", {"class": "article-index"})
-        for tag in ulTags:
-            liTags = tag.find_all("li")
-            for li in liTags:
-                a = li.find("a")
-                url_claim = a.get('href')
-                url_claim = "http://www.abc.net.au"+url_claim
-                if url_claim not in self.dict_unique_urls:
-                    self.dict_claims_category[url_claim] = category
-                    self.dict_unique_urls[url_claim] = self.claim_num
-                    self.dict_claims_urls[self.claim_num] = url_claim
-                    self.claim_num+=1
+    def get_list_claims_url(self, soup, base_url):
+        for story in soup.find_all("div", {"class": "story-headline"}):
+            a = story.find('h3').find('a')
+            url_claim = base_url + a['href']
+            category = a['href'].split('/')[1]
+            if url_claim not in self.dict_unique_urls:
+                self.dict_claims_category[url_claim] = category
+                self.dict_unique_urls[url_claim] = self.claim_num
+                self.dict_claims_urls[self.claim_num] = url_claim
+                self.claim_num += 1
 
     def clean_soup(self, soup):
         [s.extract() for s in soup('script')]
@@ -181,15 +203,20 @@ class WashingtonPost(Commons):
         # self.driver.find_element_by_id("password").send_keys("scrapping123")
         # self.driver.find_element_by_id("signinBtnTWP").click()
 
-
+        base_url = "https://www.washingtonpost.com"
         from selenium.webdriver.support.ui import WebDriverWait
 
         #Initialize webpages
         self.click_on_load_more()
         webpage_content = self.driver.page_source.encode("utf-8")
         soup = BeautifulSoup(webpage_content, 'html.parser')
-        print (len(soup.find_all("div",{"class":"story-headline"})))
-
+        self.get_list_claims_url(soup, base_url)
+        self.driver.get("https://subscribe.washingtonpost.com/loginregistration/index.html#/register/group/default?action=login&rememberme=true")
+        self.driver.find_element_by_id("login").send_keys("lcl@diku.dk")
+        self.driver.find_element_by_id("password").send_keys("scrapping123")
+        self.driver.find_element_by_id("signinBtnTWP").click()
+        time.sleep(1)
+        self.parse_claim_url()
 
         # categories = {"in-between":1}
         # for category, max in categories.items():
